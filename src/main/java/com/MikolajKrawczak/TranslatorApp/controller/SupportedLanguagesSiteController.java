1f5cbd5a469e4f0a23861d384c20a915c39b4ab9
Mikolaj Krawczak <mikolajkrawczak@MacBook-Pro-Mikolaj.local>
2020-03-01 12:29:21 +0100

    SupportedLanguagesList done

--- src/main/java/com/MikolajKrawczak/TranslatorApp/controller/SupportedLanguagesSiteController.java
+++ src/main/java/com/MikolajKrawczak/TranslatorApp/controller/SupportedLanguagesSiteController.java
@@ -2,6 +2,11 @@ package com.MikolajKrawczak.TranslatorApp.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import com.MikolajKrawczak.TranslatorApp.service.LanguagesService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,9 +14,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SupportedLanguagesSiteController {
 
+  @Autowired
+  LanguagesService languagesService;
+
   @RequestMapping("/translator-supported-languages")
-  public ModelAndView getMainSite(){
+  public ModelAndView getMainSite() throws JsonProcessingException, UnirestException {
     Map<String, Object> params = new HashMap<>();
+    params.put("languages", languagesService.getSupportedLanguages());
+
     return new ModelAndView("supported-languages-site", params);
   }
 
--- src/main/java/com/MikolajKrawczak/TranslatorApp/parser/LanguageParser.java
+++ src/main/java/com/MikolajKrawczak/TranslatorApp/parser/LanguageParser.java
@@ -1,8 +1,11 @@
 package com.MikolajKrawczak.TranslatorApp.parser;
 
+import com.MikolajKrawczak.TranslatorApp.api.LanguagesApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
@@ -18,10 +21,11 @@ public class LanguageParser {
   private ObjectMapper objectMapper = new ObjectMapper();
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  public
+  public LanguagesApi parseLanguages() throws UnirestException, JsonProcessingException {
 
-  HttpResponse<String> response = Unirest.get(String.format("https://api.cognitive.microsofttranslator.com/languages?api-version=3.0")).asString();
+    HttpResponse<String> response = Unirest.get(String.format("https://api.cognitive.microsofttranslator.com/languages?api-version=3.0")).asString();
 
-
+    return objectMapper.readValue(response.getBody(), LanguagesApi.class);
+  }
 
 }
