09647f5ab13602670429232b6fbc1b1a5f6e7149
Mikolaj-Kr <mikolak25@gmail.com>
2020-03-07 13:02:29 +0100

    Added new functionalit

--- src/main/java/com/MikolajKrawczak/TranslatorApp/controller/TranslationController.java
+++ src/main/java/com/MikolajKrawczak/TranslatorApp/controller/TranslationController.java
@@ -1,14 +1,9 @@
 package com.MikolajKrawczak.TranslatorApp.controller;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.MikolajKrawczak.TranslatorApp.api.TranslationsApi;
-import com.MikolajKrawczak.TranslatorApp.parser.TranslationParser;
 import com.MikolajKrawczak.TranslatorApp.service.LanguagesService;
 import com.MikolajKrawczak.TranslatorApp.service.TranslationsService;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -25,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-public class MainSiteController {
+public class TranslationController {
 
     @Autowired
     TranslationsService translationsService;
@@ -34,26 +29,36 @@ public class MainSiteController {
     LanguagesService languagesService;
 
     @RequestMapping("/translator-main")
-    public ModelAndView getMainSite(@RequestParam(value = "text", required = false) String text, @RequestParam(value = "from", required = false) String from, @RequestParam(value = "to", required = false) String to) throws IOException, UnirestException {
+    public ModelAndView getMainSite(@RequestParam(value = "text", required = false) String text, @RequestParam(value = "from", required = false) String from, @RequestParam(value = "to", required = false) String to, @RequestParam(value = "language", required = false) String siteLanguage, HttpServletResponse response, HttpServletRequest request) throws IOException, UnirestException {
         Map<String, Object> params = new HashMap<>();
+
+        if (siteLanguage == null){
+            siteLanguage = "en";
+        }
+
         params.put("languages", languagesService.getSupportedLanguages());
         params.put("from", from);
         params.put("to", to);
         params.put("textToTranslate", text);
         params.put("site", "main");
+        params.put("siteLanguage", siteLanguage);
+
+        request.getSession().setAttribute("siteLanguage", siteLanguage);
 
 
         if (text != null) {
           params.put("translatedText", translationsService.getTranslations(text, to, from).getTranslations().getText());
         }
-        return new ModelAndView("main-site", params);
+        if(siteLanguage.equals("pl")) {
+            return new ModelAndView("translator-pl", params);
+        } else {
+            return new ModelAndView("translator-en",params);
+        }
     }
 
     @PostMapping("/translator-main")
-    public ResponseEntity<String> translateText(@RequestParam(value = "inputText") String inputText, @RequestParam(value = "from") String from, @RequestParam(value = "to") String to, HttpServletResponse response) throws IOException, UnirestException {
-
-
-        response.sendRedirect("/translator-main?text=" + inputText + "&from=" + from +"&to=" + to);
+    public ResponseEntity<String> translateText(@RequestParam(value = "inputText") String inputText, @RequestParam(value = "from") String from, @RequestParam(value = "to") String to, HttpServletResponse response, HttpServletRequest request) throws IOException, UnirestException {
+        response.sendRedirect("/translator-main?text=" + inputText + "&from=" + from +"&to=" + to + "&language=" + request.getSession().getAttribute("siteLanguage"));
         return new ResponseEntity<>(inputText, HttpStatus.OK);
     }
 
